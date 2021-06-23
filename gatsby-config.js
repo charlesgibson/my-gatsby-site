b2cd4eae837ccc4acfc78bcb8e39e70afd1414c8
Charles E Gibson Jr <charles_gibson_jr@hotmail.com>
2021-06-23 13:28:53 -0600

    Finished Tutorial 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: "My Gatsby Site",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
